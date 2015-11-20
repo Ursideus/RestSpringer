68fcb3a10b0581d65f739e803ec15eaaa298ffa9
Waldman <Waldman>
2015-11-20 23:52:20 +0200

    Add full Entity CRUD methods

--- src/main/java/com/ursideus/OfferController.java
+++ src/main/java/com/ursideus/OfferController.java
@@ -1,5 +1,6 @@
 package com.ursideus;
 
+import ch.qos.logback.core.pattern.color.GreenCompositeConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -39,16 +40,102 @@ public class OfferController {
         offersMap.put(offer2.getId(), offer2);
 
     }
-    
+
+    private static Offer addOffer(Offer offer) {
+        offersMap.put(offer.getId(), offer);
+        return offer;
+    }
+
+    private static Offer updatOffer(Offer offer) {
+        Offer oldOffer = offersMap.get(offer.getId());
+
+        if (oldOffer == null)
+            return null;
+
+        offersMap.remove(offer.getId());
+        offersMap.put(offer.getId(), offer);
+        return offer;
+
+    }
+
+    private static boolean remove(BigInteger offerId) {
+
+        Offer deletedOffer = offersMap.remove(offerId);
+        if (deletedOffer == null)
+            return false;
+        return true;
+    }
+
+//    @RequestMapping(value = "/", method= RequestMethod.GET)
+//    public @ResponseBody Greeting sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name) {
+//        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+//    }
+
+//    @RequestMapping("/greeting")
+//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        return new Greeting(counter.incrementAndGet(),
+//                String.format(template, name));
+//    }
 
     @RequestMapping(
             value = "/api/offers",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<Offer>> getOffers() {
+    public ResponseEntity<Collection<Offer>> getAllOffers() {
         Collection<Offer> offers = offersMap.values();
 
         return new ResponseEntity<Collection<Offer>>(offers, HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            value = "/api/offers/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Offer> getOffer(@PathVariable("id") BigInteger id) {
+        Offer offer = offersMap.get(id);
+
+        if (offer == null)
+            return new ResponseEntity<Offer>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<Offer>(offer, HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            value = "/api/offers",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Offer> createOffer(@RequestBody Offer offer) {
+        Offer savedOffer = addOffer(offer);
+
+        return new ResponseEntity<Offer>(savedOffer, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(
+            value = "/api/offers/{id}",
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Offer> updateOffer(@RequestBody Offer offer) {
+        Offer updatedcOffer = updatOffer(offer);
+
+        if (updatedcOffer == null)
+            return new ResponseEntity<Offer>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return new ResponseEntity<Offer>(updatedcOffer, HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            value = "/api/offers/{id}",
+            method = RequestMethod.DELETE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+            //produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Offer> deleteOffer(BigInteger offerId) {
+        boolean isDeleted = remove(offerId);
+
+        if (!isDeleted)
+            return new ResponseEntity<Offer>(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<Offer>(HttpStatus.NO_CONTENT);
     }
 }
 
